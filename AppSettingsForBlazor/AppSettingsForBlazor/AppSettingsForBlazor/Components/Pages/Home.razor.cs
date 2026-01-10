d5d7145a0fe23033daf3eb121266eb8b164936c9
Rod Falanga <rfalanga@users.noreply.github.com>
2026-01-10 09:39:58 -0700

    Retrieve config type in Home using IAppService
    Added AppSettingsForBlazor.Services import and updated Home.razor.cs to inject IAppService. Home now fetches and stores the "ServerSide:ConfigurationType" setting asynchronously during initialization. Note: The using directive in Home.razor may need correction.

--- AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor/Components/Pages/Home.razor.cs
+++ AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor/Components/Pages/Home.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace AppSettingsForBlazor.Components.Pages
+﻿using AppSettingsForBlazor.Services;
+
+namespace AppSettingsForBlazor.Components.Pages
 {
     public partial class Home
     {
+        private string _configurationType;
+        private IAppService _appService;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _configurationType = await _appService.GetAppSettingValueAsync("ServerSide:ConfigurationType");
+        }
     }
 }
