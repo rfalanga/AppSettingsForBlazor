07171a1e3e39490c3072df580978b79429c87776
Rod Falanga <rfalanga@users.noreply.github.com>
2026-01-10 09:54:11 -0700

    Some basic formatting

--- AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor/Components/Pages/Home.razor.cs
+++ AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor/Components/Pages/Home.razor.cs
@@ -6,6 +6,7 @@ namespace AppSettingsForBlazor.Components.Pages
     public partial class Home
     {
         private string _configurationType;
+        private string _SomeValue;
 
         [Inject]
         private IAppService _appService { get; set; } = default!;
@@ -13,6 +14,7 @@ namespace AppSettingsForBlazor.Components.Pages
         protected override async Task OnInitializedAsync()
         {
             _configurationType = await _appService.GetAppSettingValueAsync("ServerSide:ConfigurationType");
+            _SomeValue = await _appService.GetAppSettingValueAsync("ServerSide:SomeValue");
         }
     }
 }
