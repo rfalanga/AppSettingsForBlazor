e41390c30f15035bb6363c3e928e5352d31e6428
Rod Falanga <rfalanga@users.noreply.github.com>
2026-01-11 16:29:12 -0700

    Trying to get the AppService (renamed WebAppService) working
    there is a problem as IHttpClientFactory is based upon
    server-side.

---
+++ AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor.Client/Services/IWebAppService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AppSettingsForBlazor.Services
+{
+    public interface IWebAppService
+    {
+        Task<string> GetAppSettingValueAsync(string key);
+    }
+}
---
+++ AppSettingsForBlazor/AppSettingsForBlazor/AppSettingsForBlazor.Client/Services/WebAppService.cs
@@ -0,0 +1,23 @@
+﻿namespace AppSettingsForBlazor.Services
+{
+    public class WebAppService : IWebAppService
+    {
+        private readonly ILogger<WebAppService> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+        // private readonly IOptionsMonitor<AppSettings> _appSettings;  // Example of using IOptionsMonitor for configuration settings, but the blog I'm following doesn't use it.
+
+        public WebAppService(ILogger<WebAppService> logger, IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GetAppSettingValueAsync(string key)
+        {
+            return _configuration[key] ?? "Key not found";
+        }
+
+    }
+}
